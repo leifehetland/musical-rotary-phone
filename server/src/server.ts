7579b477eee75efa57d0eba798a99aefd67023c1
leifehetland <lehetland1@gmail.com>
2025-02-12 17:41:37 -0500

    added wild card route to help with failing GET requests on Render

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import path from 'node:path';
+const root = process.cwd();
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
 
@@ -15,6 +17,10 @@ app.use(express.static('../client/dist'));
 
 app.use(express.json());
 app.use(routes);
+
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(root, '../client/dist/index.html'));
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
